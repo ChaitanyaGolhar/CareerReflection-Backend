dfb4deac01ef08cc3c1094d4cd9f2c7aea9c37c0
Chaitanya Golhar <golhar.241164@srttc.ac.in>
2026-07-22 13:47:53 +0530

    frontend url changed

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = parseInt(process.env.PORT ?? "3001", 10);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ?? "https://career-reflection.vercel.app",
     credentials: true,
   })
 );
